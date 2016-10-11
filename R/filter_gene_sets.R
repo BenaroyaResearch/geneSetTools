df47a3c7c3dcc2035150c3b98dccc94fb682244d
Matt Dufort <matt.dufort@gmail.com>
2016-10-11 10:19:31 -0700

    Added functions to filter low-count genes and gene sets

--- R/filter_gene_sets.R
+++ R/filter_gene_sets.R
@@ -1,25 +1,34 @@
 #' Filter gene sets
 #' 
-#' This function filters a gene set based on a set of criteria. The standard criterion is median gene expression above a certain threshold, but other functionality may be built in at a later time. 
-#' @param gene_sets the gene sets. Either a list of gene sets, with each as a character vector, or a matrix or data frame, with one gene set per column, and the gene set names as column names. Names of list elements are used as gene set names.
+#' This function filters one or more gene sets based on a set of criteria, removing gene sets that do not meet those criteria. The standard criterion is median gene expression above a certain threshold, but other functionality may be built in at a later time. It can optionally remove genes not present in the counts object, and/or remove genes from gene sets if their median counts are below a certain threshold.
+#' @param gene_sets the gene set(s). Can be one of (1) a single gene set, as character vector, (2) a list of gene sets, with each as a character vector, or (3) a matrix or data frame, with one gene set per column, and the gene set names as column names. Names of list elements or columns are used as gene set names.
 #' @param counts a matrix or data frame of counts. The dimension corresponding to genes must have dimnames, which will be matched to the gene identifiers in \code{gene_sets}.
 #' @param counts_genes_by the dimension of \code{counts} corresponding to genes. Can be "rows" or "columns" or partial matches.
-#' @param min_genes an integer, the minimum number of genes from a gene set that must be in the data. Gene sets with fewer genes overlapping with the dataset are dropped.
-#' @param min_median_expression numeric, the minimum value for median expression of genes in a gene set. Any gene sets with median expression lower than this value are removed.
-#' @param remove_missing_genes logical, whether to remove genes from the gene sets if they are not found in counts. Defaults to FALSE.
+#' @param remove_missing_genes logical, whether to remove genes from the gene sets if the genes are not present in the counts object. Defaults to TRUE.
+#' @param remove_low_count_genes logical, whether to remove genes from the gene sets if their median count does not exceed a threshold value. Defaults to FALSE.
+#' @param min_median_gene_expression numeric, the minimum value for median expression of genes. Any genes with median expression lower than this value are removed from gene sets. Defaults to 1. Ignored if \code{remove_low_count_genes} is FALSE.
+#' @param min_genes an integer, the minimum number of genes from a gene set that must be in the data. Gene sets with fewer genes overlapping with the dataset are dropped. Defaults to 2.
+#' @param min_median_gene_set_expression numeric, the minimum value for median expression of genes in a gene set. Any gene sets with median expression lower than this value are removed.
+#' @seealso gene_set_expressed
 #' @export
 #' @usage \code{filter_gene_sets(
-#' gene_sets, counts,
-#' counts_genes_by="rows",
-#' min_genes=2,
-#' min_median_expression=1,
-#' remove_missing_genes=FALSE}
+#'              gene_sets, counts,
+#'              counts_genes_by="rows",
+#'              remove_missing_genes=TRUE,
+#'              remove_low_count_genes=FALSE,
+#'              min_median_gene_expression=1,
+#'              min_genes=2,
+#'              min_median_gene_set_expression=1)}
+#' @return An object of the same class as the input object \code{gene_sets}, with the gene sets that 
 filter_gene_sets <-
   function(gene_sets, counts,
            counts_genes_by="rows",
+           remove_missing_genes=TRUE,
+           remove_low_count_genes=FALSE,
+           min_median_gene_expression=1,
            min_genes=2,
-           min_median_expression=1,
-           remove_missing_genes=FALSE) {
+           min_median_gene_set_expression=1) {
+    
     counts_genes_by <- match.arg(counts_genes_by, choices=c("rows", "columns"))
     if (counts_genes_by=="columns") counts <- t(counts)
     
@@ -34,6 +43,17 @@ filter_gene_sets <-
     }
     gene_sets <- lapply(gene_sets, na.omit) # remove NAs from each gene set
     
+    # remove missing genes if requested
+    if (remove_missing_genes)
+      gene_sets <- lapply(gene_sets, function(x) x[x %in% rownames(counts)])
+    
+    # remove low-count genes if requested
+    if (remove_low_count_genes)
+      gene_sets <- lapply(
+        gene_sets, FUN=gene_set_expressed, counts=counts,
+        remove_missing_genes=remove_missing_genes,
+        min_median_gene_expression=min_median_gene_expression)
+    
     # calculate # of genes present
     gene_sets.ngenes_present <-
       sapply(gene_sets, function(x) sum(x %in% rownames(counts)))
@@ -43,7 +63,7 @@ filter_gene_sets <-
     # filter gene sets
     gene_sets <-
       gene_sets[gene_sets.ngenes_present >= min_genes &
-                  gene_sets.median_exp >= min_median_expression]
+                  gene_sets.median_exp >= min_median_gene_set_expression]
     
     # convert back to original class, and fill in matrix or data.frame with NAs
     if (class.orig %in% c("matrix", "data.frame")) {
@@ -53,5 +73,5 @@ filter_gene_sets <-
         gene_sets <- data.frame(gene_sets)
     }
   
-    return(gene_sets)
+    gene_sets
   }
---
+++ R/gene_set_expressed.R
@@ -0,0 +1,42 @@
+#' Trim low-count genes from gene sets
+#' 
+#' This function filters a gene set based on a set of criteria. The standard criterion is median gene expression above a certain threshold, but other functionality may be built in at a later time. It can optionally remove genes not present in the counts object, and/or remove genes from gene sets if their median counts are below a certain threshold.
+#' @param gene_set the gene set, as a character vector.
+#' @param counts a matrix or data frame of counts. The dimension corresponding to genes must have dimnames, which will be matched to the gene identifiers in \code{gene_sets}.
+#' @param counts_genes_by the dimension of \code{counts} corresponding to genes. Can be "rows" or "columns" or partial matches.
+#' @param remove_missing_genes logical, whether to remove genes from the gene sets if the genes are not present in the counts object. Defaults to FALSE.
+#' @param min_genes an integer, the minimum number of genes from a gene set that must be in the data. Gene sets with fewer genes overlapping with the dataset are dropped.
+#' @param min_median_gene_set_expression numeric, the minimum value for median expression of genes in a gene set. Any gene sets with median expression lower than this value are removed.
+#' @seealso filter_gene_sets
+#' @export
+#' @usage \code{gene_set_expressed(
+#'              gene_set, counts,
+#'              counts_genes_by="rows",
+#'              remove_missing_genes=TRUE
+#'              min_median_gene_expression=1,
+#' }
+#' @return A character vector containing the genes that pass the criteria.  May be a zero-length vector, or identical to the input \code{gene_set} object.
+gene_set_expressed <-
+  function(gene_set, counts,
+           counts_genes_by="rows",
+           remove_missing_genes=TRUE,
+           min_median_gene_expression=1) {
+    if (!(is.vector(gene_set) & is.character(gene_set)))
+      stop("Input object gene_set must be a character vector.")
+    
+    counts_genes_by <- match.arg(counts_genes_by, choices=c("rows", "columns"))
+    if (counts_genes_by=="columns") counts <- t(counts)
+    
+    # remove missing genes if specified
+    if (remove_missing_genes)
+      gene_set <- gene_set[gene_set %in% rownames(counts)]
+    
+    # remove low-count genes
+    gene_set.median_exp <-
+      apply(counts[match(gene_set, rownames(counts)),],
+            MARGIN=1, median)
+    gene_set <-
+      gene_set[!is.na(gene_set.median_exp) &
+                 gene_set.median_exp >= min_median_gene_expression]
+    gene_set
+  }
